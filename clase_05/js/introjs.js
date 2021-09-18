bfbdf99650ef5679e1e447e75df3e6001f0f1114
gusmalc <gustavo@malcotti.com>
2021-09-17 21:03:10 -0300

    1

---
+++ clase_05/js/introjs.js
@@ -0,0 +1,14 @@
+let nombre = "Pedro"; // string
+let edad = 25; // number
+let vive = true; // boolean true o false
+
+//console.log(typeof vive);
+
+let a = 1;
+let b = "2.2";
+
+let x =  a.toString();
+
+//let c = parseInt(a) + parseInt(b);
+let c = parseFloat(a) + parseFloat(b);
+console.log(typeof x);
