d83be74e2fd3277f0ccf5f8b8356cea6ccae423d
gusmalc <gustavo@malcotti.com>
2021-10-01 22:13:49 -0300

    1

---
+++ clase_07/js/scripts.js
@@ -0,0 +1,39 @@
+// alert('ok');
+
+// console.log('Hola');
+
+//let clase_virtual = "1234";
+
+var claseVirtual = "1234";
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+let colores = [
+    ["Blanco", "Celeste", "Amarillo"],
+    ["Gris", "Negro", "Marron"],
+    {aa: 1, b: 2}
+];
+colores[2].aa;
+const persona = {
+    nombre: "Juan",
+    edad: 23,
+    intereses: ['cine','deporte'],
+    saluda: function(){
+        alert('Hola soy ' + this.nombre + ' y tengo ' + this.edad + " años");
+    }
+}
+
+const nomreX = persona.nombre;
+// persona.nombre;
+// let x = 'nombre';
+// personas.x;
+// persona[x];
+presonas.nombre = "Maria";
+
+persona.intereses[0];
+//console.log(colores[0].sort());
+
+
+persona.saluda();
+console.log(persona.nombre);
+
---
+++ clase_07/js/scripts2.js
@@ -0,0 +1,10 @@
+let a = "11";
+let b = 3;
+
+//let c = a%b;
+
+(a%2 === 0) ? alert('Es par')   : alert('Es impar')  ;
+
+let x = (a%2 === 0) ? 'Es par'   : 'Es impar'  ;
+
+alert(x);
